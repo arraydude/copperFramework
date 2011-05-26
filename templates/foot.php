26a4404a55dc2e68893600590fe8080df4c3df3b
ArrayDude <arraydude@gmail.com>
2011-05-26 11:49:19 -0300

    deactivate the JS SDK from config

--- templates/foot.php
+++ templates/foot.php
@@ -1,25 +1,27 @@
-    <script type="text/javascript">
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId: '<?php echo copperConfig::get('appId'); ?>',
-          status: false,
-          cookie: true,
-          xfbml: true,
-          channelUrl: '<?php echo copperConfig::get('callbackUrl'); ?>channel.html'
-        });
-        FB.getLoginStatus(function(response) {
-            if (response.session) {
-              afterFbLogin();
-            }
-        });
-        FB.Canvas.setAutoResize();
-      };
-      (function() {
-        var e = document.createElement('script');
-        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-        e.async = true;
-        document.getElementById('fb-root').appendChild(e);
-      }());
-    </script>
+    <?php if(copperConfig::get('facebookActivate')): ?>
+      <script type="text/javascript">
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId: '<?php echo copperConfig::get('appId'); ?>',
+            status: false,
+            cookie: true,
+            xfbml: true,
+            channelUrl: '<?php echo copperConfig::get('callbackUrl'); ?>channel.html'
+          });
+          FB.getLoginStatus(function(response) {
+              if (response.session) {
+                afterFbLogin();
+              }
+          });
+          FB.Canvas.setAutoResize();
+        };
+        (function() {
+          var e = document.createElement('script');
+          e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+          e.async = true;
+          document.getElementById('fb-root').appendChild(e);
+        }());
+      </script>
+    <?php endif; ?>
   </body>
 </html>

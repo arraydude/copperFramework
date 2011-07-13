c729852d9db247e82a752518386c1f5424ca0273
ArrayDude <arraydude@gmail.com>
2011-07-13 12:20:44 -0300

    Comments

--- jsVars.php
+++ jsVars.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Please dont touch this. This is the JavaScript Variable Generator.
+ * 
+ * @author Nahuel Rosso
+ * @version 1.0
+ */
+
 require_once 'config.php';
 
 header("Content-type: text/javascript");

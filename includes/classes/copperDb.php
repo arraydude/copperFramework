65355ae51dd33a3c9685c1a1648a18be52d8d87e
ArrayDude <arraydude@gmail.com>
2011-08-16 15:37:04 -0300

    New method to generate an statement

--- includes/classes/copperDb.php
+++ includes/classes/copperDb.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * copperDbPDO
  *
@@ -7,9 +8,8 @@
  * @package copperFramework
  * @author Emilio Astarita
  */
-class copperDbPDO extends PDO
-{
-
+class copperDbPDO extends PDO {
+  
 }
 
 /**
@@ -21,13 +21,12 @@ class copperDbPDO extends PDO
  * @author     Emilio Astarita
  * @version    1.0
  */
-class copperDb
-{
-  static private $instance = NULL;
-  
-  private function __construct() 
-  {
+class copperDb {
 
+  static private $instance = NULL;
+
+  private function __construct() {
+    
   }
 
   /**
@@ -39,16 +38,15 @@ class copperDb
    * @param <type> $pass
    * @return db
    */
-  static public function configure($dsn,$user,$pass)
-  {
-    if(self::$instance !== NULL) {
+  static public function configure($dsn, $user, $pass) {
+    if (self::$instance !== NULL) {
       throw new Exception('copperDb already configured.');
     }
     try {
-      self::$instance = new copperDbPDO($dsn,$user,$pass);
+      self::$instance = new copperDbPDO($dsn, $user, $pass);
       self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       self::$instance->prepare("SET NAMES UTF8;")->execute();
-    } catch(Exception $e) {
+    } catch (Exception $e) {
       copperConfig::doError(" Configure: " . $e->getMessage() . " File: " . __FILE__ . ":" . __LINE__);
       echo "Hubo un error con la base de datos.\n";
       die;
@@ -56,14 +54,13 @@ class copperDb
     return self::$instance;
   }
 
-  static public function get()
-  {
+  static public function get() {
     if (self::$instance == NULL) {
       throw new Exception('copperDb is not configured call configure static method first.');
     }
     return self::$instance;
   }
-  
+
   /**
    * Genera una cadena sql para las keys de un array
    *
@@ -71,15 +68,23 @@ class copperDb
    * @param array $array
    * @return string
    */
-  static public function generateFieldsWithKeys(array $array){
+  static public function generateFieldsWithKeys(array $array) {
     $sqlFields = implode("=?,", array_keys($array));
 
     return $sqlFields . "=?";
   }
 
-  static public function generateValues(array &$array, $values){
-    foreach($values as $value){
+  static public function generateValues(array &$array, $values) {
+    foreach ($values as $value) {
       array_push($array, $value);
     }
   }
+
+  public function statement($query, array $params = array()) {
+    $st = self::$instance->prepare($query);
+    $st->execute($params);
+    
+    return $st;
+  }
+
 }
